cee75809beb8a19087480ad28d04127acaa945c9
Yazid Ismail <flight846@gmail.com>
2016-06-17 14:03:24 +0000

    Add index page

--- app/controllers/aeroplane_controller.rb
+++ app/controllers/aeroplane_controller.rb
@@ -5,14 +5,12 @@ class AeroplaneController < ApplicationController
                 render "index_1"
             elsif @page == 2
                 render "index_2"
-            elsif @page == 2
-                render "index_2"
-            elsif @page == 2
-                render "index_2"
-            elsif @page == 2
-                render "index_2"
-            elsif @page == 2
-                render "index_2"
+            elsif @page == 3
+                render "index_3"
+            elsif @page == 4
+                render "index_4"
+            elsif @page == 5
+                render "index_5"
             else render "page"
             end
     end
